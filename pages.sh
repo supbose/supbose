736289f53c1a1e398139d431ca6f846de3b77e70
hi31089532@aliyun.com <yshxw@qq.com>
2023-12-18 13:01:04 +0800

    up

--- pages.sh
+++ pages.sh
@@ -3,23 +3,20 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# touch test.txt
-
 cur_dateTime="date +%Y-%m-%d,%H:%m:%s"
 
 echo "${cur_dateTime}" > GithubPages
 
-
+if [ -z "$Ac_TOKEN" ]; then
+  msg='mian to pages'
+  githubUrl=git@github.com:supbose/supbose.git
+else
   msg='来自github actions的自动部署'
-  # githubUrl=https://supbose:${Ac_TOKEN}@github.com/supbose/supbose.git
+  githubUrl=https://supbose:${Ac_TOKEN}@github.com/supbose/supbose.git
   git config --global user.name "supbose"
   git config --global user.email "yshxw@qq.com"
-
+fi
 git init
 git add -A
 git commit -m "${msg}"
-# git push -f $githubUrl
-# git push --all
-git remote add origin https://supbose:${Ac_TOKEN}@github.com/supbose/supbose.git
-git push -u origin master
-
+git push -f $githubUrl mian:pages
