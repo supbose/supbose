e9d536e389971b63513a8676424ed119c2b4a3f3
hi31089532@aliyun.com <yshxw@qq.com>
2023-12-18 12:32:19 +0800

    up

--- pages.sh
+++ pages.sh
@@ -3,5 +3,20 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-touch test.txt
+# touch test.txt
+
+cur_dateTime="date +%Y-%m-%d,%H:%m:%s"
+
+echo "${cur_dateTime}" > GithubPages
+
+
+  msg='来自github actions的自动部署'
+  githubUrl=https://supbose:${Ac_TOKEN}@github.com/supbose/supbose.git
+  git config --global user.name "supbose"
+  git config --global user.email "yshxw@qq.com"
+
+git init
+git add -A
+git commit -m "${msg}"
+git push -f $githubUrl
 
