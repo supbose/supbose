539df4e9e1da0f9a5fc7b2ce1a0c67e2389fb3a4
supbose <yshxw@qq.com>
2023-12-19 11:17:21 +0800

    Update pages.sh

--- pages.sh
+++ pages.sh
@@ -6,16 +6,34 @@ set -e
 cur_dateTime="date +%Y-%m-%d,%H:%m:%s"
 
 echo "${cur_dateTime}" > GithubPages
-git config --global user.name "supbose"
-git config --global user.email "yshxw@qq.com"
-githubUrl=https://supbose:${GITHUB_TOKEN}@github.com/supbose/supbose.git
-commit_info=`git describe --all --always --long`
-push_branch=pages # 推送的分支
 
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='main to pages'
+  githubUrl=git@github.com:supbose/supbose.git
+else
+  msg='来自github actions的自动部署'
+  githubUrl=https://supbose:${GITHUB_TOKEN}@github.com/supbose/supbose.git
+  git config --global user.name "supbose"
+  git config --global user.email "yshxw@qq.com"
+fi
 git init
 git add -A
-git commit -m "deploy, $commit_info"
-git push -f $githubUrl HEAD:$push_branch
+git commit -m "${msg}"
+git push -f $githubUrl main:pages # 推送到github pages分支
+
+
+
+
+# git config --global user.name "supbose"
+# git config --global user.email "yshxw@qq.com"
+# githubUrl=https://supbose:${GITHUB_TOKEN}@github.com/supbose/supbose.git
+# commit_info=`git describe --all --always --long`
+# push_branch=pages # 推送的分支
+
+# git init
+# git add -A
+# git commit -m "deploy, $commit_info"
+# git push -f $githubUrl HEAD:$push_branch
 
 
 
